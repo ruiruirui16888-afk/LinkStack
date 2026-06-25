bf1b22d224860f41b61035f7ebdf7c8f307a676e
ruiruirui16888-afk <ruiruirui16888@gmail.com>
2026-06-26 03:46:08 +0800

    Add Linktr One default category route

--- routes/linktr_one.php
+++ routes/linktr_one.php
@@ -7,6 +7,7 @@ Route::middleware(['auth', 'blocked', 'impersonate'])->group(function () {
     Route::get('/studio/linktr-one', [LinktrOneController::class, 'settings'])->name('linktr.one.settings');
     Route::post('/studio/linktr-one/style', [LinktrOneController::class, 'updateStyle'])->name('linktr.one.style.update');
     Route::post('/studio/linktr-one/category', [LinktrOneController::class, 'storeCategory'])->name('linktr.one.category.store');
+    Route::post('/studio/linktr-one/category/defaults', [LinktrOneController::class, 'seedDefaultCategories'])->name('linktr.one.category.defaults');
     Route::post('/studio/linktr-one/category/remove/{id}', [LinktrOneController::class, 'deleteCategory'])->name('linktr.one.category.remove');
     Route::post('/studio/linktr-one/share-card', [LinktrOneController::class, 'updateShareCard'])->name('linktr.one.share-card.update');
     Route::post('/studio/linktr-one/link/{id}', [LinktrOneController::class, 'updateLinkEnhancements'])->name('linktr.one.link.update');
