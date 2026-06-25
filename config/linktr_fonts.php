15a800bd4e320b6011998777eb2796415e2ed641
ruiruirui16888-afk <ruiruirui16888@gmail.com>
2026-06-26 03:32:03 +0800

    Add Linktr font configuration

---
+++ config/linktr_fonts.php
@@ -0,0 +1,172 @@
+<?php
+
+return [
+    'default' => [
+        'label' => '系统默认 / System Default',
+        'stack' => '-apple-system, BlinkMacSystemFont, "Segoe UI", "Microsoft YaHei", "PingFang SC", Arial, sans-serif',
+    ],
+    'pingfang' => [
+        'label' => '苹方 / PingFang SC',
+        'stack' => '"PingFang SC", "Hiragino Sans GB", "Microsoft YaHei", sans-serif',
+    ],
+    'microsoft_yahei' => [
+        'label' => '微软雅黑 / Microsoft YaHei',
+        'stack' => '"Microsoft YaHei", "PingFang SC", Arial, sans-serif',
+    ],
+    'noto_sans_sc' => [
+        'label' => '思源黑体 / Noto Sans SC',
+        'stack' => '"Noto Sans SC", "Source Han Sans SC", "Microsoft YaHei", sans-serif',
+    ],
+    'noto_serif_sc' => [
+        'label' => '思源宋体 / Noto Serif SC',
+        'stack' => '"Noto Serif SC", "Source Han Serif SC", "Songti SC", serif',
+    ],
+    'heiti' => [
+        'label' => '黑体 / Heiti',
+        'stack' => '"Heiti SC", "SimHei", "Microsoft YaHei", sans-serif',
+    ],
+    'songti' => [
+        'label' => '宋体 / Songti',
+        'stack' => '"Songti SC", "SimSun", "Noto Serif SC", serif',
+    ],
+    'kaiti' => [
+        'label' => '楷体 / Kaiti',
+        'stack' => '"Kaiti SC", "KaiTi", "STKaiti", serif',
+    ],
+    'fangsong' => [
+        'label' => '仿宋 / Fangsong',
+        'stack' => '"FangSong", "STFangsong", serif',
+    ],
+    'arial' => [
+        'label' => 'Arial',
+        'stack' => 'Arial, Helvetica, sans-serif',
+    ],
+    'helvetica' => [
+        'label' => 'Helvetica',
+        'stack' => 'Helvetica, Arial, sans-serif',
+    ],
+    'inter' => [
+        'label' => 'Inter',
+        'stack' => 'Inter, -apple-system, BlinkMacSystemFont, "Segoe UI", sans-serif',
+    ],
+    'roboto' => [
+        'label' => 'Roboto',
+        'stack' => 'Roboto, Arial, sans-serif',
+    ],
+    'poppins' => [
+        'label' => 'Poppins',
+        'stack' => 'Poppins, "Segoe UI", Arial, sans-serif',
+    ],
+    'montserrat' => [
+        'label' => 'Montserrat',
+        'stack' => 'Montserrat, "Segoe UI", Arial, sans-serif',
+    ],
+    'lato' => [
+        'label' => 'Lato',
+        'stack' => 'Lato, Arial, sans-serif',
+    ],
+    'nunito' => [
+        'label' => 'Nunito',
+        'stack' => 'Nunito, "Segoe UI", Arial, sans-serif',
+    ],
+    'open_sans' => [
+        'label' => 'Open Sans',
+        'stack' => '"Open Sans", Arial, sans-serif',
+    ],
+    'noto_sans' => [
+        'label' => 'Noto Sans',
+        'stack' => '"Noto Sans", Arial, sans-serif',
+    ],
+    'noto_serif' => [
+        'label' => 'Noto Serif',
+        'stack' => '"Noto Serif", Georgia, serif',
+    ],
+    'georgia' => [
+        'label' => 'Georgia',
+        'stack' => 'Georgia, "Times New Roman", serif',
+    ],
+    'times' => [
+        'label' => 'Times New Roman',
+        'stack' => '"Times New Roman", Times, serif',
+    ],
+    'playfair' => [
+        'label' => 'Playfair Display',
+        'stack' => '"Playfair Display", Georgia, serif',
+    ],
+    'merriweather' => [
+        'label' => 'Merriweather',
+        'stack' => 'Merriweather, Georgia, serif',
+    ],
+    'verdana' => [
+        'label' => 'Verdana',
+        'stack' => 'Verdana, Geneva, sans-serif',
+    ],
+    'tahoma' => [
+        'label' => 'Tahoma',
+        'stack' => 'Tahoma, Geneva, sans-serif',
+    ],
+    'trebuchet' => [
+        'label' => 'Trebuchet MS',
+        'stack' => '"Trebuchet MS", Arial, sans-serif',
+    ],
+    'gill_sans' => [
+        'label' => 'Gill Sans',
+        'stack' => '"Gill Sans", "Gill Sans MT", Calibri, sans-serif',
+    ],
+    'century_gothic' => [
+        'label' => 'Century Gothic',
+        'stack' => '"Century Gothic", AppleGothic, sans-serif',
+    ],
+    'courier' => [
+        'label' => 'Courier New',
+        'stack' => '"Courier New", Courier, monospace',
+    ],
+    'consolas' => [
+        'label' => 'Consolas',
+        'stack' => 'Consolas, Monaco, monospace',
+    ],
+    'dm_sans' => [
+        'label' => 'DM Sans',
+        'stack' => '"DM Sans", Arial, sans-serif',
+    ],
+    'work_sans' => [
+        'label' => 'Work Sans',
+        'stack' => '"Work Sans", Arial, sans-serif',
+    ],
+    'raleway' => [
+        'label' => 'Raleway',
+        'stack' => 'Raleway, Arial, sans-serif',
+    ],
+    'quicksand' => [
+        'label' => 'Quicksand',
+        'stack' => 'Quicksand, Arial, sans-serif',
+    ],
+    'oswald' => [
+        'label' => 'Oswald',
+        'stack' => 'Oswald, Arial Narrow, Arial, sans-serif',
+    ],
+    'rubik' => [
+        'label' => 'Rubik',
+        'stack' => 'Rubik, Arial, sans-serif',
+    ],
+    'futura' => [
+        'label' => 'Futura',
+        'stack' => 'Futura, "Century Gothic", Arial, sans-serif',
+    ],
+    'avenir' => [
+        'label' => 'Avenir',
+        'stack' => 'Avenir, "Avenir Next", "Segoe UI", Arial, sans-serif',
+    ],
+    'system_rounded' => [
+        'label' => '系统圆体 / System Rounded',
+        'stack' => 'ui-rounded, "SF Pro Rounded", "PingFang SC", "Microsoft YaHei", sans-serif',
+    ],
+    'seravek' => [
+        'label' => 'Seravek',
+        'stack' => 'Seravek, "Segoe UI", Arial, sans-serif',
+    ],
+    'optima' => [
+        'label' => 'Optima',
+        'stack' => 'Optima, Candara, "Noto Sans", sans-serif',
+    ],
+];
